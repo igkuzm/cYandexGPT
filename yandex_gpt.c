634d6cc730a489188a34deff0249d2b286949b2c
Igor V. Sementsov <kuzmich@macbook.kuzm>
2024-04-15 11:52:12 +1000

    summary

--- yandex_gpt.c
+++ yandex_gpt.c
@@ -8,11 +8,13 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2){
 		printf(
-				"Ask Yandex GPT for something...\n"
+				"As Yndex GPT for someting...\n"
 				"set C_YANDEX_GPT_ID and C_YNDEX_GPT_API_KEY "
 				"as ENV or in ~/.cYandexGPT.conf\n"
-				"Usage: \n"
-				"\t%s \"TEXT\"\n", argv[0]);
+				"\n"
+				"usage: %s [options] \"TEXT\"\n"
+				"  -s  --summary\tsummarize the text\n"
+				"  if TEXT is '-'\tuse stdin\n", argv[0]);
 		return 0;
 	}
 
@@ -41,8 +43,53 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	char *str = 
-		c_yandex_gpt_ask_for(argv[1], id, api_key);
+	// check options
+	int summary = 0;
+	char *text = argv[1];
+	if (
+			strcmp(text, "-s") == 0 ||
+			strcmp(text, "--summary") == 0
+			)
+	{
+		text = argv[2];
+		summary = 1;
+	}
+
+	// check stdin
+	if (strcmp(text, "-") == 0) {
+		text = malloc(1);
+		if (!text)
+			return -1;
+		// read from stdin 
+		int i,   // iterator 
+				k=0, // buflen
+				l=0, // textlen
+				n=0; // number of buffers
+		char buf[BUFSIZ];
+		while ((
+					k=fread(
+						buf, 1, BUFSIZ, stdin)))
+		{
+			n++;
+			// realloc text
+			text = realloc(text, n*BUFSIZ + 1);
+			if (!text)
+				return -1;
+
+			// copy buffer to text
+			for (i = 0; i < k; ++i)
+				text[l++] = buf[i];	
+		}
+
+		text[l] = 0;
+	}
+
+	char *str = NULL;
+	if (summary) 
+		str = c_yandex_gpt_summary(text, id, api_key);
+	else
+		str = c_yandex_gpt_ask_for(text, id, api_key);
+	
 	if (str) {
 		printf("%s\n", str);
 	}
